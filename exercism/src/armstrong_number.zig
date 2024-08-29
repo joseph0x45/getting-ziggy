55de89b87678e7eeea94072dee0c2e578c3cac92
joseph0x45 <zozozozeph@gmail.com>
2024-08-29 13:01:25 +0000

    use a more clever solution with bufPrint

--- exercism/src/armstrong_number.zig
+++ exercism/src/armstrong_number.zig
@@ -1,21 +1,15 @@
 const std = @import("std");
 
 pub fn isArmstrongNumber(num: u128) bool {
-    if (num < 10) {
-        return true;
-    }
-    const allocator = std.heap.page_allocator;
-    const num_str: []u8 = std.fmt.allocPrint(allocator, "{}", .{num}) catch unreachable;
-    defer allocator.free(num_str);
-    const num_of_digits: usize = num_str.len;
+    var buffer: [64]u8 = undefined;
+    const num_str = std.fmt.bufPrint(&buffer, "{d}", .{num}) catch unreachable;
+    std.debug.print("{s}", .{buffer});
+    std.debug.print("{s}", .{num_str});
     var sum: u128 = 0;
     for (num_str) |char| {
-        const char_str: []u8 = std.fmt.allocPrint(allocator, "{c}", .{char}) catch unreachable;
-        const char_int: u128 = std.fmt.parseInt(u128, char_str, 10) catch unreachable;
-        sum += std.math.pow(u128, char_int, @as(u128, num_of_digits));
-        allocator.free(char_str);
+        const value = char - '0';
+        sum += std.math.powi(u128, value, num_str.len) catch unreachable;
     }
-    std.debug.print("{}", .{sum});
     return sum == num;
 }
 
