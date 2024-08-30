fe7428c0962b1ade02192b9ed1d18edc4128dcbc
joseph0x45 <zozozozeph@gmail.com>
2024-08-30 20:19:22 +0000

    hamming distance

---
+++ exercism/src/hamming.zig
@@ -0,0 +1,9 @@
+pub const DnaError = error{};
+
+pub fn compute(first: []const u8, second: []const u8) DnaError!usize {
+    _ = first;
+    _ = second;
+    @compileError("please implement the compute function");
+}
+
+pub fn main() void {}
